93fa2e46abbf71aa5b5ab0ce20c8ddba1c46c74d
federicopalma <federicopalma92@outlook.it>
2017-04-19 09:46:53 +0200

    aggiuntafile

---
+++ Scripts/install_jmeter.sh
@@ -0,0 +1,9 @@
+{
+	sudo apt-get update
+	sudo apt-get install -y default-jre
+	sudo apt-get install -y jmeter
+	cd /usr/share/jmeter
+	sudo wget https://jmeter-plugins.org/files/packages/jpgc-perfmon-2.1.zip
+	sudo unzip jpgc-perfmon-2.1.zip
+	sudo rm jpgc-perfmon-2.1.zip
+}
